b4d3dd0b9f8834cfcb1199d553d92e0c90bfac6f
BalaPriyaMS <balapriya@intellicar.in>
2024-06-26 17:56:07 +0530

    update delete fetch the data from api

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,69 @@ import "./App.css";
 import Model from "./components/Model";
 import Table from "./components/Table";
 import { useState, useEffect } from "react";
-import apiRequest from "./api/apiRequest";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import EditModel from "./components/EditModel";
 
 function App() {
+  const url = "http://localhost:3500/data";
   const [data, setData] = useState([]);
   const navigation = useNavigate();
 
   useEffect(() => {
-    const fetchMethod = () => {};
+    const fetchMethod = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setData(data);
+    };
+    fetchMethod();
   }, []);
 
-  const handleDelete = (id) => {
-    const updatedData = data.filter((item) => item.user_id !== id);
-    setData(updatedData);
+  const handleDelete = async (id) => {
+    console.log(id);
+    console.log(`${url}/${id}`);
+    try {
+      await fetch(`${url}/${id}`, {
+        method: "DELETE",
+      });
+      const updatedData = data.filter((item) => item.user_id !== id);
+      setData(updatedData);
+    } catch (e) {
+      e.message;
+    }
   };
 
-  const handlePost = (newdata) => {
-    setData((prev) => [...prev, newdata]);
-    navigation("/");
+  const handlePost = async (newdata) => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newdata),
+      });
+      const data = await response.json();
+      setData((prev) => [...prev, data]);
+      navigation("/");
+    } catch (e) {
+      console.log(e.message);
+    }
   };
 
-  const handleUpdate = (id, updateData) => {
-    const updateDatas = data.map((item) =>
-      item.user_id === updateData.user_id ? updateData : item
-    );
-    setData(updateDatas);
-    navigation("/");
+  const handleUpdate = async (id, updateData) => {
+    try {
+      const response = await fetch(`${url}/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(updateData),
+      });
+      const data = response.json();
+      const updateDatas = data.map((item) =>
+        item.user_id === updateData.user_id ? data : item
+      );
+      setData(updateDatas);
+      navigation("/");
+    } catch (e) {
+      console.e("Failed to update data");
+    }
   };
 
   return (
